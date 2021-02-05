d6264911902750e48362efeef54ea1e296b8f28e
Devon Layton <dml4h@mtmail.mtsu.edu>
2021-02-05 16:45:02 -0500

    updated routes and nav bar

--- src/app/appHeader/AppHeader.tsx
+++ src/app/appHeader/AppHeader.tsx
@@ -5,14 +5,54 @@ import {
   FormControl,
   Nav,
   Navbar,
+  Jumbotron,
   NavDropdown,
   OverlayTrigger,
   Tooltip,
   Image,
 } from "react-bootstrap";
+import { Github, Linkedin } from "react-bootstrap-icons";
+import logo from "src/app/customLogo512.png";
+import "./AppHeader.scss";
 
 const AppHeader: React.FC = (): ReactElement => {
-  return <>Header</>;
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="home">
+        <Image src={logo} className="App-logo" alt="logo" style={{
+            padding: "10px",
+          }}/>{" "}
+        <Navbar.Brand
+          style={{
+            paddingLeft: "16px",
+          }}
+        >
+          Devon Layton
+        </Navbar.Brand>
+      </Navbar.Brand>
+
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link
+            className="Nav-link"
+            href="https://github.com/Dml4h/DevonLayton/tree/master"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Github />
+          </Nav.Link>
+          <Nav.Link
+            className="Nav-link"
+            href="https://www.linkedin.com/in/devonlayton017/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Linkedin />
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default AppHeader;
